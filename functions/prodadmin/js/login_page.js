4f5caed50854b15603794ad25de04af6f3648d22
rohithaider <rohithaider@gmail.com>
2020-02-05 10:35:54 -0600

    login sign in page

--- functions/prodadmin/js/login_page.js
+++ functions/prodadmin/js/login_page.js
@@ -1,7 +1,70 @@
-function login_page(){
 
 
-glPageContent.innerHTML='<h1> Login Page </h1>'
+function login_page() {
 
 
+    firebase.auth().onAuthStateChanged (user =>{
+
+        if(user && user.email === 'prodadmin@test.com'){
+
+            window.location.href= '/home'
+        }else{
+
+            glPageContent.innerHTML=
+            `
+            
+            <form class = "form-signin">
+                <h3>Please Sign In </h3>
+                 <input type="email" class="form-control" id="email" placeholder="Email Address">
+    
+  
+                <input type="password" class="form-control" id="password" placeholder = "Password">
+  
+                <button type="button" class="btn btn-primary" onclick="signIn()">Submit</button>
+           </form>
+            
+            
+            
+            `
+
+        }
+
+
+
+
+    })
+
+
+    
+
+
+}
+
+
+async function signIn(){
+
+
+    try {
+
+        const email = document.getElementById('email').value
+        if(email !== 'prodadmin@test.com'){
+            throw new Error('Not product admin')
+        }
+
+        const password = document.getElementById('password').value
+        await firebase.auth().signInWithEmailAndPassword(email,password)
+
+        window.location.href = '/home'
+    } catch(e) {
+        glPageContent.innerHTML=`
+        
+        Login Failed: <br>
+        ${e}
+        <br>
+        <a href = "/login" class="btn-outline-primary">Go to Login </a>
+        
+        `;
+        
+        
+    }
 }
